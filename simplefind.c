8d81a8c1ac38a5cb8d331d8140e8fa8fe3e3dd68
Marco-Chen-06 <marcochen.work@gmail.com>
2025-10-03 02:11:21 -0400

    implemented xdev flag functionality

--- simplefind.c
+++ simplefind.c
@@ -1,7 +1,5 @@
 #include "simplefind.h"
 
-#include "simplefind.h"
-
 int print_info(bool ls_flag, bool xdev_flag, bool name_flag, char *name_pattern, char *starting_path, char *buffer, struct dirent *nextdir) {
 	if (name_flag) {
 		if (fnmatch(name_pattern, nextdir->d_name, 0) == 0) {
@@ -13,7 +11,16 @@ int print_info(bool ls_flag, bool xdev_flag, bool name_flag, char *name_pattern,
 }
 
 int recursive_dfs_search(bool ls_flag, bool xdev_flag, bool name_flag, char *name_pattern, char *starting_path) {
-	DIR *currentdir = opendir(starting_path);
+	DIR *currentdir;
+	if ((currentdir = opendir(starting_path)) == NULL) {
+		fprintf(stderr, "Failed to open path %s: %s\n", starting_path, strerror(errno));
+	}
+
+	// get the stat struct for the starting path
+	struct stat start_st;
+	if (stat(starting_path, &start_st) == -1) {
+		fprintf(stderr, "Failed to get stat struct for path: %s. %s \n", starting_path, strerror(errno)); 
+	}
 
 	if (currentdir == NULL) {
 		return -1;
@@ -23,14 +30,28 @@ int recursive_dfs_search(bool ls_flag, bool xdev_flag, bool name_flag, char *nam
 	char buffer[1024];
 
 	while(true) {
-		nextdir = readdir(currentdir);
+		if (((nextdir = readdir(currentdir)) == NULL) && (errno != 0)) {
+			fprintf(stderr, "Failed to read directory %s: %s\n", starting_path, strerror(errno)); 
+		}
 
 		if (nextdir == NULL) {
 			break;
 		}
 
+		snprintf(buffer, 1024, "%s/%s", starting_path, nextdir->d_name);
+
+		// in the case xdev_flag is set, skip directories on filesystems different from starting_path's
+		if (xdev_flag) {
+			struct stat next_st;
+			if (stat(buffer, &next_st) == -1) {
+				fprintf(stderr, "Failed to get stat struct for path: %s. %s \n", buffer, strerror(errno));
+			}
+			if (start_st.st_dev != next_st.st_dev) {
+				continue;
+			}
+		}
+
 		if (strcmp(nextdir->d_name, ".") != 0 && strcmp(nextdir->d_name, "..") != 0) {
-			snprintf(buffer, 1024, "%s/%s", starting_path, nextdir->d_name);
 			print_info(ls_flag, xdev_flag, name_flag, name_pattern, starting_path, buffer, nextdir);
 			// printf("%s\n", buffer);
 			if (nextdir->d_type == DT_DIR) {
